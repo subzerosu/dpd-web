cdb2aa7dbff2bd67bbb0be2c50fabbb27e5e36fe
mniedre <webcane@ya.ru>
2016-09-21 23:34:56 +0300

    runtime ex

--- DpdClient/src/main/java/cane/brothers/spring/dpd/exception/DpdCalculationException.java
+++ DpdClient/src/main/java/cane/brothers/spring/dpd/exception/DpdCalculationException.java
@@ -1,10 +1,7 @@
 package cane.brothers.spring.dpd.exception;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
 
-@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class DpdCalculationException extends RuntimeException {
 
 	private static final long serialVersionUID = 3800222299886969036L;
--- DpdClient/src/main/java/cane/brothers/spring/dpd/exception/DpdConnectionException.java
+++ DpdClient/src/main/java/cane/brothers/spring/dpd/exception/DpdConnectionException.java
@@ -3,7 +3,7 @@ package cane.brothers.spring.dpd.exception;
 /**
  * Created by cane on 14.09.16.
  */
-public class DpdConnectionException extends Exception {
+public class DpdConnectionException extends RuntimeException {
 
 	private static final long serialVersionUID = 4516253435978900032L;
 
