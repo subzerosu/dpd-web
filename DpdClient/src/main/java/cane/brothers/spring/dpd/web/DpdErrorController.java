7cf291646430df142294647a82d7f44a8509d48e
mniedre <webcane@ya.ru>
2016-09-22 20:54:32 +0300

    use proxy autoconfig

--- DpdClient/src/main/java/cane/brothers/spring/dpd/web/DpdErrorController.java
+++ DpdClient/src/main/java/cane/brothers/spring/dpd/web/DpdErrorController.java
@@ -1,5 +1,9 @@
 package cane.brothers.spring.dpd.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +17,13 @@ public class DpdErrorController implements ErrorController {
     private static final String PATH = "/error";
 
 
+    //@RequestMapping(value = PATH)
+//    public String error() {
+//        return "redirect:/dpd/";
+//    }
     @RequestMapping(value = PATH)
-    public String error() {
-        return "Error handling";
+    void handleFoo(HttpServletResponse response) throws IOException {
+      response.sendRedirect("/");
     }
 
 
